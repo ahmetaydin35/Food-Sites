ca0fdc798397de4e8726bc885e3ab13fb2ff3d49
Ahmet Aydin <ahm3t35@gmail.com>
2024-02-17 22:16:56 +0300

    cart context

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,21 +1,17 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-export default function Modal({ children, open, onClose, className = '' }) {
+export default function Modal({ children, open, className = '' }) {
   const dialog = useRef();
 
   useEffect(() => {
-    const modal = dialog.current;
-
     if (open) {
-      modal.showModal();
+      dialog.current.showModal();
     }
-
-    return () => modal.close();
   }, [open]);
 
   return createPortal(
-    <dialog ref={dialog} className={`modal ${className}`} onClose={onClose}>
+    <dialog ref={dialog} className={`modal ${className}`}>
       {children}
     </dialog>,
     document.getElementById('modal')
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,7 +4,6 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
-  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -50,10 +49,6 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
-  if (action.type === 'CLEAR_CART') {
-    return { ...state, items: [] };
-  }
-
   return state;
 }
 
@@ -68,15 +63,10 @@ export function CartContextProvider({ children }) {
     dispatchCartAction({ type: 'REMOVE_ITEM', id });
   }
 
-  function clearCart() {
-    dispatchCartAction({ type: 'CLEAR_CART' });
-  }
-
   const cartContext = {
     items: cart.items,
     addItem,
-    removeItem,
-    clearCart
+    removeItem
   };
 
   return (
